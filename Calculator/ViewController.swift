cdcc8049d9a4d5c48a9bcd992bc6d2565deda44a
Павло Тимощук <Pavlo25575@icloud.com>
2020-02-11 14:42:23 +0200

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Павло Тимощук on 11.02.2020.
+//  Copyright © 2020 Павло Тимощук. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
